0c397561e6990d37ff4c47a3958bbb3c5d5a66dd
pblo <pdiazarenas541@gmail.com>
2026-01-16 00:17:59 +0000

    implement  methods

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const fs  = require('fs')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 
-const app =express()
-const port = 3000
-
-
+const app = express();
+const port = 3000;
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //rutas
-app.get('/', (req,res)=>{
-    res.status(200).sendFile(path.join(__dirname, 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "index.html"));
+});
 
+app.get("/canciones", (req, res) => {
+  try {
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf-8"));
+    res.status(200).json(canciones);
+  } catch (error) {
+    console.log("Error del servidor", error);
+  }
+});
+app.post("/canciones", (req, res) => {
+  try {
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf-8"));
 
+    if (!cancion) {
+      res.status(404).send("Tu canción no existe o no ha sido encontrada");
+    }
 
-app.listen(port, ()=>{
-    console.log(`Servidor activo en http://localhost:${port}`)
-})
+    canciones.push(cancion);
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones, null, 2));
+
+    res.status(200).json({ message: "Canción añadida con éxito" });
+  } catch (error) {
+    console.log("Error del servidor", error);
+  }
+});
+app.put("/canciones:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf-8"));
+    const index = canciones.findIndex((item) => item.id == id);
+    canciones[index] = cancion;
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones, null, 2));
+    res.status(200).json({ message: "Canción modificada con éxito" });
+  } catch (error) {
+    console.log("Error del servidor", error);
+  }
+});
+app.delete("/canciones:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf-8"));
+    const index = canciones.findIndex((item) => item.id == id);
+    canciones.splice(index, 1);
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones, null, 2));
+    res.status(200).json({ message: "Canción borrada con éxito" });
+  } catch (error) {
+    console.log("Error del servidor", error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor activo en http://localhost:${port}`);
+});
